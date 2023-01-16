2ef8d4def5e4606d8053c8e25e37316f665c4b64
tlqkrus012345 <tlqkrus012345@naver.com>
2023-01-16 13:36:29 +0900

    Initial commit

---
+++ src/main/java/com/sym/SymApplication.java
@@ -0,0 +1,13 @@
+package com.sym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sym/SymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
