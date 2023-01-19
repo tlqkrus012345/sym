d761001c4eb71d15a7e046a7cbc00aa22311a06e
tlqkrus012345 <tlqkrus012345@naver.com>
2023-01-19 21:54:11 +0900

    [#1] 회원 엔티티 작성

---
+++ src/main/java/com/sym/member/Member.java
@@ -0,0 +1,24 @@
+package com.sym.member;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Builder;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String email;
+    private String nickName;
+    private String password;
+    private int point;
+
+    @Builder
+    public Member(String email, String nickName, String password) {
+        this.email = email;
+        this.nickName = nickName;
+        this.password = password;
+    }
+}
