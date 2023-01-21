4816c2b3126e1cbf003d3306ad41916baeda5ef6
tlqkrus012345 <tlqkrus012345@naver.com>
2023-01-21 13:44:47 +0900

    [#2] 회웝 가입 Dto 클래스 작성

---
+++ src/main/java/com/sym/member/dto/MemberRegisterRequestDto.java
@@ -0,0 +1,29 @@
+package com.sym.member.dto;
+
+import com.sym.member.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class MemberRegisterRequestDto {
+
+    private Long id;
+    private String email;
+    private String nickName;
+    private String password;
+
+    @Builder
+    public MemberRegisterRequestDto(String email, String nickName, String password) {
+        this.email = email;
+        this.nickName = nickName;
+        this.password = password;
+    }
+    public Member toEntity() {
+        return Member.builder()
+            .email(email)
+            .nickName(nickName)
+            .password(password)
+            .build();
+    }
+}
