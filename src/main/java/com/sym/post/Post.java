2e52f90cc240ea4addaff35be1113eb18dcf6083
tlqkrus012345 <tlqkrus012345@naver.com>
2023-01-30 14:40:51 +0900

    [#5] 포스트 도메인 클래스 작성

---
+++ src/main/java/com/sym/post/Post.java
@@ -0,0 +1,16 @@
+package com.sym.post;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private Long id;
+    private String title;
+    private String text;
+    private String hashtag;
+
+    private String writer;
+    private LocalDateTime createDate;
+    private String modifier;
+    private LocalDateTime modifyDate;
+
+}
