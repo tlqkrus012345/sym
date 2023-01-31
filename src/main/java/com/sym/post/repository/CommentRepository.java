83d9cb555ab122839bb33e1e9159b3e92cce8dc8
tlqkrus012345 <tlqkrus012345@naver.com>
2023-01-31 18:28:40 +0900

    [#12] Data Rest 설정
    댓글 INSERT 쿼리 작성
    게시판, 댓글 json API를 자동으로 RESTFUL하게 만들게 설정

--- src/main/java/com/sym/post/repository/CommentRepository.java
+++ src/main/java/com/sym/post/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.sym.post.repository;
 
 import com.sym.post.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
