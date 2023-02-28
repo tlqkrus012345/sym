e03bb1938b06ed996409a8bc5bb42c87a7be8106
tlqkrus012345 <tlqkrus012345@naver.com>
2023-02-28 21:49:34 +0900

    [#53] 심리검사 결과 엔티티 작성

---
+++ src/main/java/com/sym/sympathy/domain/PsychologicalSurvey.java
@@ -0,0 +1,42 @@
+package com.sym.sympathy.domain;
+
+import com.sym.member.domain.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.asm.Advice;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "survey")
+@Entity
+public class PsychologicalSurvey {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "survey_id")
+    private Long id;
+    @OneToOne(mappedBy = "survey", fetch = FetchType.LAZY)
+    private Member member;
+    private String surveyResult;
+    private String depression;
+    private String angerControlDisorder;
+    private String insanity;
+    @CreatedDate
+    private LocalDateTime surveyDateTime;
+    @LastModifiedDate
+    private LocalDateTime modifyDateTime;
+
+    private PsychologicalSurvey(Member member,String surveyResult, String depression, String angerControlDisorder, String insanity) {
+        this.depression = depression;
+        this.angerControlDisorder = angerControlDisorder;
+        this.insanity = insanity;
+        this.member = member;
+    }
+    public static PsychologicalSurvey of(Member member, String surveyResult, String depression, String angerControlDisorder, String insanity) {
+        return new PsychologicalSurvey(member, surveyResult, depression, angerControlDisorder, insanity);
+    }
+}
